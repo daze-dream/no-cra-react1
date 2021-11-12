8db19ed72b6b23f12024ff4fbd22fc289cc68d61
CheetoDustNeo <misterrolypoly@gmail.com>
2021-11-12 12:55:12 -0600

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log('JS is running');
+var template = /*#__PURE__*/React.createElement("p", null, "CATS");
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('JS is running')
+
+var template = <p>CATS</p>
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template,appRoot )
